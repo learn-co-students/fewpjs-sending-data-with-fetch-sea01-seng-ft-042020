728c0bdeaa748375ad2d6e792b05a1e7122c7f07
ckaiser258 <kaiserc2@seattleu.edu>
2020-06-04 15:56:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      let div = document.createElement("div");
+      document.body.appendChild(div);
+      div.innerHTML = object.id;
+    })
+    .catch(function (error) {
+      alert("error");
+      let div = document.createElement("div");
+      document.body.appendChild(div);
+      div.innerHTML = error.message;
+    });
+}
